80983a067faf56878d16e3144eb384a55bb0abc3
Roger Philippe Pereira <rogerphilippe@outlook.com>
2019-07-26 18:43:04 -0300

    Initial commit

---
+++ src/main/java/br/com/philippesis/macaddress/Main.java
@@ -0,0 +1,36 @@
+package br.com.philippesis.macaddress;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            // get all network interfaces of the current system
+            Enumeration<NetworkInterface> networkInterface = NetworkInterface.getNetworkInterfaces();
+            // iterate over all interfaces
+            while (networkInterface.hasMoreElements()) {
+                // get an interface
+                NetworkInterface network = networkInterface.nextElement();
+                // get its hardware or mac address
+                byte[] macAddressBytes = network.getHardwareAddress();
+                if (macAddressBytes != null) {
+                    System.out.print("MAC address of interface \"" + network.getName() + "\" is : ");
+                    // initialize a string builder to hold mac address
+                    StringBuilder macAddressStr = new StringBuilder();
+                    // iterate over the bytes of mac address
+                    for (int i = 0; i < macAddressBytes.length; i++) {
+                        // convert byte to string in hexadecimal form
+                        macAddressStr.append(String.format("%02X", macAddressBytes[i])+":");
+                        // check if there are more bytes, then add a "-" to make it more readable
+                    }
+                    System.out.println(macAddressStr.toString());
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
